68f845e565f5bfff4b2956d7f3d31bdd180a7170
agustintosco <agustintosco.92@gmail.com>
2022-02-14 23:38:09 -0300

    added event, sport, betoption entities n betopt enum

---
+++ src/events/models/event.entity.ts
@@ -0,0 +1,58 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { BetOption } from './bet-option.entity';
+import { Sport } from './sport.entity';
+
+@Entity('events')
+export class Event {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Sport, { eager: true })
+  @JoinColumn({ name: 'sport_id' })
+  sport: number;
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+  })
+  name: string;
+
+  @Column({
+    name: 'started',
+    type: 'boolean',
+    default: false,
+  })
+  started: boolean;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp',
+    default: null,
+  })
+  deletedAt: Date;
+
+  @OneToMany(() => BetOption, (betOption) => betOption.event, { eager: true })
+  betOptions: BetOption[];
+}
