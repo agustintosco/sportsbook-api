8ccbeaf30aa677c91e9704fa7ecdc7b4529213ca
agustintosco <agustintosco.92@gmail.com>
2022-02-16 15:29:35 -0300

    created dto for user editing

--- src/users/models/edit-user.dto.ts
+++ src/users/models/edit-user.dto.ts
@@ -1,3 +1,60 @@
-import { User } from './user.entity';
+import { IsEmail, IsIn, IsNumber, IsOptional, IsString } from 'class-validator';
+import { Role } from './roles.enum';
+import { UserState } from './user-state.enum';
 
-export class EditUserDTO implements Partial<User> {}
+export class EditUserDTO {
+  @IsOptional()
+  @IsString()
+  firstName: string;
+
+  @IsOptional()
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsNumber()
+  phone: string;
+
+  @IsOptional()
+  @IsEmail(
+    {},
+    {
+      message: 'EMAIL_NOT_VALID',
+    },
+  )
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  address: string;
+
+  @IsOptional()
+  @IsString()
+  gender: string;
+
+  @IsOptional()
+  @IsString()
+  countryId: string;
+
+  @IsOptional()
+  @IsString()
+  city: string;
+
+  @IsOptional()
+  @IsString()
+  category: string;
+
+  @IsOptional()
+  @IsString()
+  documentId: string;
+}
+
+export class EditUserDTOAdmin extends EditUserDTO {
+  @IsOptional()
+  @IsIn([Role.ADMIN, Role.USER])
+  role: Role;
+
+  @IsOptional()
+  @IsIn([UserState.ACTIVE, UserState.BLOCKED])
+  userState: UserState;
+}
