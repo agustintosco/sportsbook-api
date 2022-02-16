7edf31a5ece82f97317518bd2cd8370737518c95
agustintosco <agustintosco.92@gmail.com>
2022-02-16 15:35:55 -0300

    added event emitter n method for bet result

--- src/events/services/bet-options.service.ts
+++ src/events/services/bet-options.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+
 import { BetOption } from '../models/bet-option.entity';
 import { CreateBetOptionDTO } from '../models/create-bet-option.dto';
+import { SetBetOptionResultDTO } from '../models/set-bet-option-resutl.dto';
 
 @Injectable()
 export class BetOptionService {
   constructor(
     @InjectRepository(BetOption)
     private betOptionRepository: Repository<BetOption>,
+    private eventEmitter: EventEmitter2,
   ) {}
 
   async create(createBetOptionDTO: CreateBetOptionDTO) {
@@ -17,5 +21,19 @@ export class BetOptionService {
     );
 
     await this.betOptionRepository.save(betOption);
+  }
+
+  async setBetOptionResult(betOptionResultDTO: SetBetOptionResultDTO) {
+    let betOption: BetOption = await this.betOptionRepository.findOne(
+      betOptionResultDTO.id,
+    );
+
+    if (betOption) {
+      betOption.result = betOptionResultDTO.result;
+
+      await this.betOptionRepository.save(betOption);
+
+      this.eventEmitter.emit('bet-option.result-set', betOption);
+    }
   }
 }
