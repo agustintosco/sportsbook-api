bac0b65a1cea40b1dbb7cc191d84a67775694366
agustintosco <agustintosco.92@gmail.com>
2022-02-16 15:36:38 -0300

    added role decorator n patch method

--- src/events/controllers/bet-option.controller.ts
+++ src/events/controllers/bet-option.controller.ts
@@ -1,14 +1,25 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Patch, Post } from '@nestjs/common';
 import { BetOptionService } from '../services/bet-options.service';
 import { CreateBetOptionDTO } from '../models/create-bet-option.dto';
+import { Roles } from './../../auth/decorators/roles.decorator';
+import { Role } from 'src/users/models/roles.enum';
+import { SetBetOptionResultDTO } from '../models/set-bet-option-resutl.dto';
 
-@Controller('events')
+@Controller('events/bet-options')
 export class BetOptionController {
   constructor(private readonly betOptionService: BetOptionService) {}
 
-  @Post('bet-options')
+  @Roles(Role.ADMIN)
+  @Post()
   @HttpCode(201)
   async create(@Body() createBetOptionDTO: CreateBetOptionDTO) {
     await this.betOptionService.create(createBetOptionDTO);
+  }
+
+  @Roles(Role.ADMIN)
+  @Patch()
+  @HttpCode(201)
+  async setBetOptionResult(@Body() betOptionResultDTO: SetBetOptionResultDTO) {
+    await this.betOptionService.setBetOptionResult(betOptionResultDTO);
   }
 }
