7a42119e3530c4c7866a4cc2671d4b699a9548ff
agustintosco <agustintosco.92@gmail.com>
2022-02-17 20:32:23 -0300

    added doc to auth co

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,27 @@
 import { Controller, Post, UseGuards, Body, Req } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
+
 import { CreateUserDTO } from './../users/models/create-user.dto';
 import { LogInUserDTO } from './../users/models/login-user.dto';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { Public } from './decorators/public.decorator';
 import { AuthService } from './auth.service';
 
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiCreatedResponse({
+    description: 'The user has been successfully registered.',
+  })
+  @ApiBadRequestResponse({
+    description: 'One or more properties are missing or are wrong.',
+  })
   @Public()
   @Post('register')
   async register(@Body() user: CreateUserDTO) {
@@ -17,6 +30,12 @@ export class AuthController {
     await this.authService.create(user);
   }
 
+  @ApiCreatedResponse({
+    description: 'User logged.',
+  })
+  @ApiBadRequestResponse({
+    description: 'One or more properties are missing or are wrong.',
+  })
   @UseGuards(LocalAuthGuard)
   @Public()
   @Post('login')
