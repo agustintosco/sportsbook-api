5b32587096ca71069a49321866484590d0b4b59d
agustintosco <agustintosco.92@gmail.com>
2022-02-17 20:37:03 -0300

    added doc to bet-option dtos

--- src/events/models/create-bet-option.dto.ts
+++ src/events/models/create-bet-option.dto.ts
@@ -1,12 +1,31 @@
 import { IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBetOptionDTO {
+  @ApiProperty({
+    name: 'name',
+    description: 'Name of the Bet Option',
+    type: String,
+    examples: ['Palmeiras', 'Draw', 'Corinthians'],
+  })
   @IsString()
   name: string;
 
+  @ApiProperty({
+    name: 'odd',
+    description: 'Odd assigned to the bet option',
+    type: Number,
+    example: 1.5,
+  })
   @IsNumber()
   odd: number;
 
+  @ApiProperty({
+    name: 'event',
+    description: 'Event ID related to the Bet Option',
+    type: Number,
+    example: 3,
+  })
   @IsNumber()
   event: number;
 }
