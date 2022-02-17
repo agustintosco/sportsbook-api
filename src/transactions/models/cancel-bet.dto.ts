da73eff676718c5c0112f36249a77b207ebbbc17
agustintosco <agustintosco.92@gmail.com>
2022-02-17 20:37:25 -0300

    added doc to bet n transaction dtos

--- src/transactions/models/cancel-bet.dto.ts
+++ src/transactions/models/cancel-bet.dto.ts
@@ -1,10 +1,24 @@
 import { IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 import { Bet } from './bet.entity';
 
 export class CancelBetDTO {
+  @ApiProperty({
+    name: 'eventId',
+    description: 'Event ID related to the Bet',
+    type: Number,
+    example: 2,
+  })
   @IsNumber()
   eventId: number;
 
+  @ApiProperty({
+    name: 'bet',
+    description: 'Bet ID to be canceled',
+    type: Number,
+    example: 2,
+  })
   @IsNumber()
   bet: Bet;
 }
--- src/transactions/models/create-bet.dto.ts
+++ src/transactions/models/create-bet.dto.ts
@@ -1,14 +1,34 @@
 import { IsNumber, IsPositive } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 import { BetOption } from './../../events/models/bet-option.entity';
 
 export class CreateBetDTO {
+  @ApiProperty({
+    name: 'eventId',
+    description: 'Event ID related to the Bet',
+    type: Number,
+    example: 2,
+  })
   @IsNumber()
   eventId: number;
 
+  @ApiProperty({
+    name: 'amount',
+    description: 'Bet amount',
+    type: Number,
+    example: 150,
+  })
   @IsNumber()
   @IsPositive()
   amount: number;
 
+  @ApiProperty({
+    name: 'betOption',
+    description: 'Bet Option ID on which the Bet will be set',
+    type: Number,
+    example: 2,
+  })
   @IsNumber()
   betOption: BetOption;
 }
