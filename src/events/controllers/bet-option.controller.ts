574afe67e34e03b2a40e89caadd509b0607e2046
agustintosco <agustintosco.92@gmail.com>
2022-02-17 20:34:26 -0300

    added doc to bet-option co

--- src/events/controllers/bet-option.controller.ts
+++ src/events/controllers/bet-option.controller.ts
@@ -1,14 +1,29 @@
 import { Body, Controller, HttpCode, Patch, Post } from '@nestjs/common';
+import {
+  ApiOkResponse,
+  ApiTags,
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
+
 import { BetOptionService } from '../services/bet-options.service';
 import { CreateBetOptionDTO } from '../models/create-bet-option.dto';
 import { Roles } from './../../auth/decorators/roles.decorator';
 import { Role } from 'src/users/models/roles.enum';
 import { SetBetOptionResultDTO } from '../models/set-bet-option-resutl.dto';
 
+@ApiTags('Bet Options')
 @Controller('events/bet-options')
 export class BetOptionController {
   constructor(private readonly betOptionService: BetOptionService) {}
 
+  @ApiBearerAuth()
+  @ApiOkResponse({
+    description: 'The bet option has been successfully created.',
+  })
+  @ApiBadRequestResponse({
+    description: 'One or more properties are missing or are wrong.',
+  })
   @Roles(Role.ADMIN)
   @Post()
   @HttpCode(201)
@@ -16,6 +31,13 @@ export class BetOptionController {
     await this.betOptionService.create(createBetOptionDTO);
   }
 
+  @ApiBearerAuth()
+  @ApiOkResponse({
+    description: 'The bet option result has been successfully updated.',
+  })
+  @ApiBadRequestResponse({
+    description: 'One or more properties are missing or are wrong.',
+  })
   @Roles(Role.ADMIN)
   @Patch()
   @HttpCode(201)
