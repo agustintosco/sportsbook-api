0fb67e69ab461b320579f636e26ea4528883e73a
agustintosco <agustintosco.92@gmail.com>
2022-02-17 20:35:15 -0300

    added doc to entities

--- src/events/models/sport.entity.ts
+++ src/events/models/sport.entity.ts
@@ -6,34 +6,54 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
 
 @Entity('sports')
 export class Sport {
+  @ApiProperty({
+    name: 'id',
+    type: Number,
+    readOnly: true,
+    example: 1,
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    name: 'name',
+    description: 'Name of the sport to be created',
+    type: String,
+    example: 'Basketball',
+  })
   @Column({
     name: 'name',
     type: 'varchar',
   })
   name: string;
 
+  @ApiHideProperty()
   @CreateDateColumn({
     name: 'created_at',
     type: 'timestamp',
   })
+  @Exclude({ toPlainOnly: true })
   createdAt: Date;
 
+  @ApiHideProperty()
   @UpdateDateColumn({
     name: 'updated_at',
     type: 'timestamp',
   })
+  @Exclude({ toPlainOnly: true })
   updatedAt: Date;
 
+  @ApiHideProperty()
   @DeleteDateColumn({
     name: 'deleted_at',
     type: 'timestamp',
     default: null,
   })
+  @Exclude({ toPlainOnly: true })
   deletedAt: Date;
 }
