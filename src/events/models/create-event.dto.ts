b2f14ae2f933fcca83266b0f6465e6702f30f268
agustintosco <agustintosco.92@gmail.com>
2022-02-17 20:37:39 -0300

    added doc to event dtos

--- src/events/models/create-event.dto.ts
+++ src/events/models/create-event.dto.ts
@@ -1,9 +1,22 @@
 import { IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateEventDTO {
+  @ApiProperty({
+    name: 'name',
+    description: 'Name of the event',
+    type: String,
+    example: 'Palmeiras vs Corinthians',
+  })
   @IsString()
   name: string;
 
+  @ApiProperty({
+    name: 'sport',
+    description: 'Sport ID related to the event',
+    type: Number,
+    example: 2,
+  })
   @IsNumber()
   sport: number;
 }
